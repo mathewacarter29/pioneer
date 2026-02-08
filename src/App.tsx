c17c3dd4b5dfbd9a9dcd83ed6705d49eb3b1e7f8
Mathew Carter <mathewacarter@gmail.com>
2026-02-08 17:51:12 -0500

    color buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
-import Board from './components/Board/Board'
+import "./App.css";
+import Board from "./components/Board/Board";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#444544"
+    }
+  }
+});
 
 function App() {
-
   return (
-    <div>
-      <h2>Pioneer</h2>
-      <Board />
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <div>
+        <h2>Pioneer</h2>
+        <Board />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
